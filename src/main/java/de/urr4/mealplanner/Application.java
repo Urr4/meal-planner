8741d127924b6cc310636e97b4df184f04c70dcf
Stefan Schubert <stefan.schubert@prodyna.com>
2021-08-10 22:34:50 +0200

    Add test root get-endpoint

--- src/main/java/de/urr4/mealplanner/Application.java
+++ src/main/java/de/urr4/mealplanner/Application.java
@@ -2,12 +2,21 @@ package de.urr4.mealplanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @GetMapping(produces = MediaType.TEXT_PLAIN_VALUE)
+    public String test(){
+        return "ok";
     }
 
 }
