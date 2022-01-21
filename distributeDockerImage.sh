b5d4053841069665ba9e72b4e0513685ea0c95d7
Stefan Schubert <stefan.schubert@prodyna.com>
2022-01-21 09:22:39 +0100

    Adds github workflow

--- distributeDockerImage.sh
+++ distributeDockerImage.sh
@@ -1,2 +1,11 @@
-docker buildx build --platform linux/arm/v7 -t 08021986/meal-planner-be:v1 . --push
-kubectl get pods -n dev --no-headers=true | awk '/meal-planner-be/{print $1}' | xargs kubectl delete pod -n dev
+#!/bin/bash
+
+if [ -z "$1" ]
+  then
+    echo "Image tag argument needed"
+    exit 1
+fi
+
+echo "Tagging image version $1"
+docker buildx build --platform linux/arm/v7 -t 08021986/meal-planner-be:$1 . #--push
+#kubectl get pods -n dev --no-headers=true | awk '/meal-planner-be/{print $1}' | xargs kubectl delete pod -n dev
